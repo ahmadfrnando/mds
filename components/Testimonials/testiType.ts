665760fb8875598f66c60b7565f43943f0355371
ahmadfrnando <ahmadfernando88@gmail.com>
2024-06-19 23:32:25 +0700

    tambah testimoni dan responsive

--- components/Testimonials/testiType.ts
+++ components/Testimonials/testiType.ts
@@ -10,46 +10,46 @@ export const TestimonialData: Testimonial[] = [
     id: 1,
     name: "Sarah A.",
     text:
-      "Belanja di MDS sangat memuaskan! Produk-produk yang ditawarkan berkualitas tinggi dan pengirimannya sangat cepat. Saya pasti akan kembali berbelanja di sini. Terima kasih!",
+      "Menggunakan jasa pembuatan website dari MDS sangat memuaskan! Website yang dibuat sangat berkualitas dan pengerjaannya sangat cepat. Saya pasti akan kembali menggunakan jasa ini. Terima kasih!",
     img: "https://picsum.photos/101/101",
   },
   {
     id: 2,
     name: "Budi",
-    text: "Saya sangat senang dengan layanan yang diberikan. Customer service-nya sangat responsif dan membantu saya memilih produk yang tepat. Pengalaman belanja yang sangat menyenangkan!",
+    text: "Saya sangat senang dengan layanan yang diberikan. Customer service-nya sangat responsif dan membantu saya memilih desain yang tepat. Pengalaman bekerja sama yang sangat menyenangkan!",
     img: "https://picsum.photos/102/102",
   },
   {
     id: 3,
     name: "Mira Khusnul",
     text:
-      "Produk yang saya beli di MDS persis seperti yang diiklankan. Kualitasnya luar biasa dan harganya sangat terjangkau. Saya merekomendasikan toko ini kepada semua teman saya!",
+      "Website yang dibuat oleh MDS persis seperti yang diharapkan. Kualitasnya luar biasa dan harganya sangat terjangkau. Saya merekomendasikan jasa ini kepada semua teman saya!",
     img: "https://picsum.photos/103/103",
   },
   {
     id: 4,
     name: "Rizky",
     text:
-      "Pengiriman super cepat dan produk datang dalam kondisi sempurna. Sungguh pengalaman belanja online yang luar biasa. Terima kasih, MDS",
+      "Pengerjaan super cepat dan hasilnya datang dalam kondisi sempurna. Sungguh pengalaman bekerja sama yang luar biasa. Terima kasih, MDS",
     img: "https://picsum.photos/104/104",
   },
   {
     id: 5,
     name: "Dewi",
     text:
-      "Ini pertama kalinya saya belanja di MDS dan saya sangat puas. Proses pemesanan sangat mudah dan saya mendapatkan produk dalam waktu singkat. Pasti akan belanja lagi!",
+      "Ini pertama kalinya saya menggunakan jasa pembuatan website dari MDS dan saya sangat puas. Proses pemesanan sangat mudah dan saya mendapatkan hasil dalam waktu singkat. Pasti akan menggunakan jasa ini lagi!",
     img: "https://picsum.photos/105/105",
   },
   {
     id: 6,
     name: "Andi",
-    text: "Saya suka berbelanja di sini karena koleksi produknya lengkap dan selalu up-to-date. Harga yang ditawarkan juga kompetitif dan sering ada promo menarik. Luar biasa!",
+    text: "Saya suka menggunakan jasa MDS karena koleksi desainnya lengkap dan selalu up-to-date. Harga yang ditawarkan juga kompetitif dan sering ada promo menarik. Luar biasa!",
     img: "https://picsum.photos/106/106",
   },
   {
     id: 7,
     name: "Rina",
-    text: "Produk yang saya beli sangat sesuai dengan deskripsinya. Kualitasnya sangat baik dan saya sangat puas dengan pembelian ini. Pelayanannya juga sangat ramah dan cepat!",
+    text: "Website yang dibuat sangat sesuai dengan deskripsinya. Kualitasnya sangat baik dan saya sangat puas dengan hasil ini. Pelayanannya juga sangat ramah dan cepat!",
     img: "https://picsum.photos/107/107",
   },
 ];
