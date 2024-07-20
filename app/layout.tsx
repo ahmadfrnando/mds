ce36125061046bfd58436106f41d0e34c681637f
ahmadfrnando <ahmadfernando88@gmail.com>
2024-07-20 17:55:19 +0700

    update lagi seonya

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://medanmds.web.app"),
-  keywords: ["jasa website medan", "jasa website murah", "jasa website company profile", "jasa website toko online", "jasa website e commerce"],
+  keywords: ["jasa website medan", "jasa website murah", "jasa website company profile", "jasa website toko online","mds","medan digital solution", "jasa website e commerce"],
   title: "Medan Digital Solution",
   description: "Jasa website medan",
 };
